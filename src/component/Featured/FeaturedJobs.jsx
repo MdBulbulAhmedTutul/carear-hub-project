4c65d072d65a0fffc2a7c6cc657f4952a66ef915
MdBulbulAhmedTutul <tutulwebdevoloper@gmail.com>
2023-09-22 14:37:21 -0700

    show all btn hidden

--- src/component/Featured/FeaturedJobs.jsx
+++ src/component/Featured/FeaturedJobs.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import Job from "../Job/Job";
+// import { data } from "autoprefixer";
 
 const FeaturedJobs = () => {
     const [jobs, setJobs] = useState([]);
+
+    const [dataLength, setDataLength] = useState(4);
     useEffect(() => {
         fetch('jobs.json')
             .then(res => res.json())
@@ -16,8 +19,13 @@ const FeaturedJobs = () => {
             </div>
             <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-7 mb-16">
                 {
-                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                    jobs.slice(0, dataLength).map(job => <Job key={job.id} job={job}></Job>)
                 }
+            </div>
+            <div className="justify-center flex mb-16">
+                <div className={dataLength === jobs.length && 'hidden'}>
+                    <button onClick={() => setDataLength(jobs.length)} className="bg-[#7E90FE]  px-4 py-2 rounded-lg font-semibold text-white">Show All Jobs</button>
+                </div>
             </div>
         </div>
     );
